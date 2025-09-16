6d751c091bdc72960e8d410fb92e0f73630be236
José Manuel <josdevjs@gmail.com>
2025-09-16 04:47:10 -0600

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const requestLogger = require("./requestLogger");
 
 function createId() {
@@ -9,7 +8,7 @@ function createId() {
 
 const app = express();
 
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan(requestLogger));
 
